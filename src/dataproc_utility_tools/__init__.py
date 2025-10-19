935dfb187509d365e3db5438755a7e698f15d42a
rodrigo98gt <rs9789@gmail.com>
2025-10-19 19:29:18 -0300

    fix dependencies issues

--- src/dataproc_utility_tools/__init__.py
+++ src/dataproc_utility_tools/__init__.py
@@ -6,15 +6,16 @@ and dataset cleaning.
 """
 
 __version__ = "0.1.0"
-__author__ = "Your Name"
+__author__ = "Rodrigo Ribeiro"
 
 from .schema_analyzer import SchemaAnalyzer
 from .clean_dataset import CleanDatasetProcessor
-from .utils import schema_from_analysis_json, _map_inferred_type_to_dtype
+from .utils import schema_from_analysis_json, _map_inferred_type_to_dtype, schema_to_pyspark_struct
 
 __all__ = [
     "SchemaAnalyzer",
     "CleanDatasetProcessor", 
     "schema_from_analysis_json",
-    "_map_inferred_type_to_dtype"
+    "_map_inferred_type_to_dtype",
+    "schema_to_pyspark_struct"
 ]
